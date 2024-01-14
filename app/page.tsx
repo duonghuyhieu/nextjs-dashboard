eca30a6b36e9aca75e9314897004252e80d69dbd
Duong Huy Hieu <gunnyhuyhieu@gmail.com>
2024-01-14 23:58:33 +0700

    hi

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ export default function Page() {
     return (
         // These are Tailwind classes:
         <main className="flex min-h-screen flex-col p-6">
-            <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-                <AcmeLogo />
+            <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+                {/* Add Hero Images Here */}
                 <Image
                     src="/hero-desktop.png"
                     width={1000}
